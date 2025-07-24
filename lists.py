6827429cc8a4312528abe2236681a549fd7c78e2
Prithvi-alt382 <prithvichauhan2443@gmail.com>
2025-07-24 21:10:57 +0530

    Add example code for lists and tuples with student data

---
+++ lists.py
@@ -0,0 +1,12 @@
+#Lists and Tuples 
+"""
+marks = [ 94.4, 95.5 , 96.2, 66.4 , 45.1]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[1])
+print(len(marks)) 
+"""
+student = ["Rohan" , 22 , "Engineering" , 95.7]
+student[0] = "Shyam"
+print(student[3])
