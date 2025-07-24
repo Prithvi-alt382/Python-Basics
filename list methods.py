159cff79c1bc3df789fa171b2fa9fce6cda9c0de
Prithvi-alt382 <prithvichauhan2443@gmail.com>
2025-07-24 21:29:23 +0530

    Update list methods example to include sorting and reversing operations

--- list methods.py
+++ list methods.py
@@ -1,5 +1,24 @@
 # These are methods used in lists
 List1 = [ 2, 2, 3]
+"""
 List1.append(4)
 print(List1)
 
+List1.sort()
+print(List1)
+
+List1.sort(reverse=True)
+print(List1)
+
+
+List1.reverse()
+print(List1)
+"""
+List1.insert(0 , 5)
+print(List1)
+
+List1.remove(2)
+print(List1)
+
+List1.pop(2)
+print(List1)
